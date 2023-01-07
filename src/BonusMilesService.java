508f200761a6db05b09bab953f788d920f18523c
Anastasia I <niavolk@gmail.com>
2023-01-07 17:28:39 +0400

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int amountOfRubles = 20; //количество рублей для 1 бонусной мили
+
+        int amountOfMiles = cost/amountOfRubles;
+        return amountOfMiles;
+    }
+}
