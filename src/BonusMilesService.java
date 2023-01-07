8beaf5860c3e126b537c5363150c10ef8fd79806
Anastasia I <niavolk@gmail.com>
2023-01-07 23:42:50 +0400

    Reformated Code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,7 @@
 public class BonusMilesService {
     public int calculate(int cost) {
         int amountOfRubles = 20; //количество рублей для 1 бонусной мили
-
-        int amountOfMiles = cost/amountOfRubles;
+        int amountOfMiles = cost / amountOfRubles;
         return amountOfMiles;
     }
 }
